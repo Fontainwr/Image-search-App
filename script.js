c724a65c6f888b6e06d44f7d9318e97a5a8fd5c7
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2023-08-21 14:56:31 +0900

    Added javascript code.
    Added javascript code.

---
+++ script.js
@@ -0,0 +1,8 @@
+const accessKey = "AX2OzyRm5gxgE7FImP167YbVcG8QU4OV7X9hqQl5zgQ"
+const formEl = document.querySelector("form")
+const inputEl = document.getElementById("search-input")
+const searchResults = document.querySelector(".search-results")
+const showMore = document.getElementById(".show-more-button")
+
+let inputData = ""
+let page = 1;
